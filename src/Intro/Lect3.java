2ecb19baaf4fa271a894ef6ce00c537bf670f71b
sAt4 <roksatish8690@gmail.com>
2023-03-01 22:54:58 +0530

    Lect3 is Completed

--- src/Intro/Lect3.java
+++ src/Intro/Lect3.java
@@ -5,6 +5,7 @@ public class Lect3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lect 3 initiated");
+		System.out.println("Lect 3 completed");
 	}
 
 }
